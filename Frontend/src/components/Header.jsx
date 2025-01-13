6f0b116f1b019972520e5eddf20bbc91ab9afb71
MohammadSazzad <sazzad19@student.sust.edu>
2025-01-13 21:35:09 +0600

    Logout Functionality added

--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -1,13 +1,21 @@
 import { AiOutlineShoppingCart } from "react-icons/ai";
 import { useNavigate } from "react-router-dom";
+import { jwtDecode } from "jwt-decode";
 const Header = () => {
 
     const navigate = useNavigate();
 
     const handleLoginButton = () => {
-        console.log("Login button clicked");
         navigate("/customer/login");
     }
+
+    const handleProfileButton = () => {
+        navigate("/customer/profile");
+    }
+
+    const isLogin = localStorage.getItem("token");
+    const token = isLogin?jwtDecode(isLogin):{};
+    const img = token.image;
 
     return(
         <header className="p-3 text-bg-dark fixed-top">
@@ -21,8 +29,16 @@ const Header = () => {
 
                 <div className="text-end">
                 <button type="button" className="btn btn-outline-light me-5">{<AiOutlineShoppingCart/>}</button>
-                <button type="button" className="btn btn-outline-light me-2" onClick={handleLoginButton}>Login</button>
-                <button type="button" className="btn btn-warning">Sign-up</button>
+                {isLogin ? (
+                    <button type="button" className="btn p-0 border border-black rounded-circle " onClick={handleProfileButton}>{
+                        <img src={img} alt="Profile" style={{width: '40px', height: '40px', borderRadius: '50%'}} />
+                    }</button>
+                ) : (
+                    <>
+                        <button type="button" className="btn btn-outline-light me-2" onClick={handleLoginButton}>Login</button>
+                        <button type="button" className="btn btn-warning">Sign-up</button>
+                    </>
+                )}
                 </div>
             </div>
             </div>
